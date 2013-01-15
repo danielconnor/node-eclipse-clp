20f2ff38256b195852b8c0ad684e55f681367dd7
Dan <danielconnr@gmail.com>
2013-01-15 17:24:53 +0000

    add new constructors for atom and functor types

--- src/functor.h
+++ src/functor.h
@@ -9,16 +9,17 @@ class Functor : public EC_functor, public node::ObjectWrap {
   static v8::Persistent<v8::FunctionTemplate> template_;
   static v8::Persistent<v8::Function> constructor;
 
-  static void Init(v8::Handle<v8::Object> target);
-  static v8::Handle<v8::Value> NewInstance(v8::Handle<v8::Value> name, v8::Handle<v8::Value> arity);
+  static void Init(v8::Handle<v8::Object>);
+  static v8::Handle<v8::Value> NewInstance(char *, unsigned int);
+  static v8::Handle<v8::Value> NewInstance(EC_word&) {
 
  private:
 
-  Functor(char *name, int arity) : EC_functor(name, arity) {}
+  Functor(char *name, int arity) : EC_functor(name, arity), ObjectWrap() {}
 
-  static v8::Handle<v8::Value> New(const v8::Arguments& args);
-  static v8::Handle<v8::Value> getName(v8::Local<v8::String> property, const v8::AccessorInfo &info);
-  static v8::Handle<v8::Value> getArity(v8::Local<v8::String> property, const v8::AccessorInfo &info);
+  static v8::Handle<v8::Value> New(const v8::Arguments&);
+  static v8::Handle<v8::Value> getName(v8::Local<v8::String>, const v8::AccessorInfo&);
+  static v8::Handle<v8::Value> getArity(v8::Local<v8::String>, const v8::AccessorInfo&);
 
 };
 
