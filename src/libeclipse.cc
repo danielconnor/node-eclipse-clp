63368da9bb3ee9cb75709bc0eb3209e3ff50486f
danielconnor <danielconnr@gmail.com>
2013-01-16 00:53:33 +0000

    seperate post_goal and term functions

--- src/libeclipse.cc
+++ src/libeclipse.cc
@@ -48,48 +48,36 @@ Handle<Value> resume(const Arguments& args) {
   return scope.Close(Number::New(result));
 }
 
-#define GET_ARG(INDEX)                                             \
-  if(args[INDEX]->IsString()) {                                    \
-    ec_args[INDEX - 1] = EC_word(*String::Utf8Value(args[INDEX])); \
-  }                                                                \
-  else if(args[INDEX]->IsObject()) {                               \
-    if(Atom::template_->HasInstance(args[INDEX])) {                \
-      ec_args[INDEX - 1] = EC_word(*ObjectWrap::Unwrap<Atom>(      \
-        args[INDEX]->ToObject()));                                 \
-    }                                                              \
-    else if(Ref::template_->HasInstance(args[INDEX])) {            \
-      ec_args[INDEX - 1] = EC_word(*ObjectWrap::Unwrap<Ref>(       \
-        args[INDEX]->ToObject()));                                 \
-    }                                                              \
+
+Handle<Value> term(const Arguments& args) {
+  HandleScope scope;
+
+  EC_word *ec_args = new EC_word[args.Length() - 1];
+
+  if(Functor::template_->HasInstance(args[0])) {
+
+    Functor *functor = ObjectWrap::Unwrap<Functor>(
+          args[0]->ToObject());
+
+    for(int i = 1; i < args.Length(); i++) {
+      ec_args[i - 1] = jsToProlog(args[i]);
+    }
+
+    EC_word compound = term(*functor, *ec_args);
+
+    delete[] ec_args;
+
+    return scope.Close(Compound::NewInstance(compound));
   }
 
+  return scope.Close(Undefined());
+}
 
 Handle<Value> post_goal(const Arguments& args) {
   HandleScope scope;
 
   if(args[0]->IsObject()) {
-
-    EC_word *ec_args = new EC_word[args.Length() - 1];
-
-    if(Functor::template_->HasInstance(args[0])) {
-
-      Functor *functor = ObjectWrap::Unwrap<Functor>(
-            args[0]->ToObject());
-
-      for(int i = 1; i < args.Length(); i++) {
-        GET_ARG(i);
-      }
-
-      post_goal(term(*functor, *ec_args));
-
-      delete[] ec_args;
-    }
-    else if(Atom::template_->HasInstance(args[0])) {
-      Atom *atom = ObjectWrap::Unwrap<Atom>(
-            args[0]->ToObject());
-
-      post_goal(*atom);
-    }
+    post_goal(jsToProlog(args[0]));
   }
   else if(args[0]->IsString()) {
     post_goal(*String::Utf8Value(args[0]));
@@ -110,6 +98,10 @@ void Init(Handle<Object> target) {
 
   target->Set(String::NewSymbol("post_goal"),
     FunctionTemplate::New(post_goal)->GetFunction());
+
+  target->Set(String::NewSymbol("term"),
+    FunctionTemplate::New(term)->GetFunction());
+
 
   Functor::Init(target);
   Ref::Init(target);
